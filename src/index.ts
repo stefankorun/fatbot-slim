641d397b433cbb8fa7d41c38d7e4792c4df7c9c4
korun <stefankorun@outlook.com>
2021-08-27 15:50:52 +0200

    Extract split init() method in App

--- src/index.ts
+++ src/index.ts
@@ -19,26 +19,16 @@ class App {
   ) {}
 
   async init() {
-    // Initialize Discord client.
     await this.discordClient.init();
+    await this.registerHandlers();
+    await this.handleMusicMessage();
+  }
 
-    // Register commands.
-    const mainGuild = await this.groobyClient.getMainGuild();
-    if (mainGuild) {
-      await this.commandHandler.register(mainGuild);
-    } else {
-      console.error('Could not find Guild to attach commands to.');
-    }
-
-    // Register command and message handlers.
-    this.discordClient.on(
-      'interactionCreate',
-      async (interaction) => await this.commandHandler.handle(interaction)
-    );
-    this.discordClient.on('messageCreate', async (message) =>
-      console.log('Received message:', message.content)
-    );
-
+  /**
+   * React to commands starting with '>play' in order to start playing a song.
+   * @deprecated Refactor candidate.
+   */
+  async handleMusicMessage() {
     this.discordClient.on('messageCreate', async (message) => {
       if (message.content.startsWith('>play') === false) return;
 
@@ -60,6 +50,27 @@ class App {
 
       this.musicPlayer.subscribeToConnection(connection);
     });
+  }
+
+  async registerHandlers() {
+    // Register bot commands.
+    const mainGuild = await this.groobyClient.getMainGuild();
+    if (mainGuild) {
+      await this.commandHandler.register(mainGuild);
+    } else {
+      console.error('Could not find Guild to attach commands to.');
+    }
+
+    // Register command and message handlers.
+    this.discordClient.on(
+      'interactionCreate',
+      async (interaction) => await this.commandHandler.handle(interaction)
+    );
+
+    // Replay message on receive.
+    this.discordClient.on('messageCreate', async (message) =>
+      console.log('Received message:', message.content)
+    );
   }
 }
 

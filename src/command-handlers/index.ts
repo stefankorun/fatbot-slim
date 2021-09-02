7183eeb561d02f2701282a4d003055278b92100e
Hristijan Dimitrieski <hristijan.dimitrieski@gmail.com>
2021-09-02 21:20:06 +0200

    Pause and resume commands (#8)
    * Fix skip handling
    
    * Ability to pause and resume songs
    
    Co-authored-by: Hristijan Dimitrieski <Hristijan.Dimitrieski@netcetera.com>

--- src/command-handlers/index.ts
+++ src/command-handlers/index.ts
@@ -1,5 +1,7 @@
 import { IWillBeFamousHandler } from './i-will-be-famous-handler';
+import { PauseHandler } from './pause-handler';
 import { PlayHandler } from './play-handler';
+import { ResumeHandler } from './resume-handler';
 import { SkipSongHandler } from './skip-song-handler';
 import { StopHandler } from './stop-handler';
 
@@ -8,4 +10,6 @@ export default [
   StopHandler.commandData,
   SkipSongHandler.commandData,
   PlayHandler.commandData,
+  PauseHandler.commandData,
+  ResumeHandler.commandData,
 ];
---
+++ src/command-handlers/resume-handler.ts
@@ -0,0 +1,20 @@
+import { GroovyCommand } from './groovy-command';
+import { Command, commandHandler, CommandHandler } from '../command-bus';
+import { ApplicationCommandData, CommandInteraction } from 'discord.js';
+import { MusicPlayer } from '../music-player';
+
+@commandHandler(GroovyCommand.Resume)
+export class ResumeHandler implements CommandHandler {
+  static readonly commandData: ApplicationCommandData = {
+    name: GroovyCommand.Resume,
+    description: 'Resumes the player'
+  };
+
+  constructor(private readonly musicPlayer: MusicPlayer) {
+  }
+
+  async handle(command: Command<CommandInteraction>) {
+    await this.musicPlayer.resume();
+    await command.payload.reply('Resumed');
+  }
+}

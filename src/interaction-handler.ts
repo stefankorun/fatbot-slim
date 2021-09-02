3f1890a1fa7706cf9154ba11e50b2911358fd52f
Stefan <stefankorun@outlook.com>
2021-09-02 12:20:34 +0200

    Ability to add command prefix (#6)
    Easier debugging with command prefix for local bots.
    - Adds DISCORD_PREFIX env variable, used for prefixing all registered commands

--- src/interaction-handler.ts
+++ src/interaction-handler.ts
@@ -2,23 +2,33 @@ import { Guild, Interaction } from 'discord.js';
 import { injectable } from 'tsyringe';
 import { CommandBus } from './command-bus';
 import commands from './command-handlers';
+import { ConfigurationService } from './services/configuration';
 
 @injectable()
 export class InteractionHandler {
-
-  constructor(private readonly commandBus: CommandBus) {
-  }
+  constructor(
+    private readonly commandBus: CommandBus,
+    private config: ConfigurationService
+  ) {}
 
   async register(guild: Guild) {
-    return guild.commands.set(commands);
+    return guild.commands.set(
+      commands.map((command) => ({
+        ...command,
+        name: `${this.config.get('DISCORD_PREFIX')}_${command.name}`,
+      }))
+    );
   }
 
   async handle(interaction: Interaction) {
     if (!interaction.isCommand() || !interaction.guildId) return;
 
     this.commandBus.execute({
-      type: interaction.commandName,
-      payload: interaction
+      type: interaction.commandName.replace(
+        `${this.config.get('DISCORD_PREFIX')}_`,
+        ''
+      ),
+      payload: interaction,
     });
   }
 }

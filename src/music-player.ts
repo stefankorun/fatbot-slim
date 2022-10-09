4f80887ded4fcc7c9793a30bcd32fafb4c9d9de1
Stefan <stefankorun@outlook.com>
2022-10-09 21:06:01 +0200

    Bump audio quality

--- src/music-player.ts
+++ src/music-player.ts
@@ -49,7 +49,7 @@ export class MusicPlayer {
     // `ytdl` settings taken from https://github.com/fent/node-ytdl-core/issues/902#issuecomment-1086880966
     const stream = ytdl(url, {
       filter: 'audioonly',
-      quality: 'lowestaudio',
+      quality: 'highestaudio',
       highWaterMark: 1 << 62,
       liveBuffer: 1 << 62,
       dlChunkSize: 0,
